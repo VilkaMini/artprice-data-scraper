70a979b8380c73b7308f1c7b5bc294f181d9958e
VilkaMini <alaunisvilius@gmail.com>
2022-05-09 19:15:45 +0300

    Updated except clause

--- src/artscraper.py
+++ src/artscraper.py
@@ -69,7 +69,7 @@ class Artscraper:
             try:
                 sec_name = (row.find("td", class_="section").text).strip()
                 sec_value = (row.find("td", class_="value").text).strip()
-            except:
+            except AttributeError:
                 break
             # Checking all possible keys
             if sec_name == "Title":
