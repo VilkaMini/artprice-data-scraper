c51ed4d9f6cfc4a0e2d84ee843f004110b9e12a2
VilkaMini <alaunisvilius@gmail.com>
2022-05-11 20:36:23 +0300

    Version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="artScrape",
-    version="0.1.0",
+    version="0.1.1",
     description="Artprice scraper if designed to be used to scrape data from Artprice.com",
     py_modules=["artscraper"],
     package_dir={"": "src"},
